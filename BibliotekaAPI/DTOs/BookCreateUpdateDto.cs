9ace0ec5aaf7aee5d0b128978d0c98ad841797a8
Jakub Krzemień <jakub9141@gmail.com>
2025-12-15 09:23:37 +0100

    Update BookCreateUpdateDto.cs

--- BibliotekaAPI/DTOs/BookCreateUpdateDto.cs
+++ BibliotekaAPI/DTOs/BookCreateUpdateDto.cs
@@ -6,15 +6,11 @@ namespace BibliotekaAPI.DTOs
     public class BookCreateUpdateDto
     {
         [Required(ErrorMessage = "Tytuł książki jest wymagany.")]
-        // Brak JsonPropertyName, oczekuje "Title" lub "title"
         public string Title { get; set; } = null!;
 
         [Required(ErrorMessage = "Rok publikacji jest wymagany.")]
-        // Brak JsonPropertyName, oczekuje "Year" lub "year"
         public string Year { get; set; } = null!;
 
-        // KOREKTA: Brak JsonPropertyName. Oczekuje "AuthorId" lub "authorId" (camelCase).
-        // (W przeciwnym razie deserializacja zawsze zawiedzie)
         [Required(ErrorMessage = "ID autora jest wymagane.")]
         public string AuthorId { get; set; } = null!;
     }
