8c723e6ea774fe0f78ebdf45bfb049485620191f
Jakub Krzemień <jakub9141@gmail.com>
2025-12-15 09:23:21 +0100

    Update AuthorDto.cs

--- BibliotekaAPI/DTOs/AuthorDto.cs
+++ BibliotekaAPI/DTOs/AuthorDto.cs
@@ -9,10 +9,9 @@ namespace BibliotekaAPI.DTOs
         public int? Id { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Imię jest wymagane.")]
-        [JsonPropertyName("first_name")] // Zgodne z testem JS
+        [JsonPropertyName("first_name")] 
         public string FirstName { get; set; } = null!;
 
-        // KOREKTA: Wymusza Minimalną długość 1, aby puste nazwisko ("") zwróciło 400.
         [StringLength(255, MinimumLength = 1, ErrorMessage = "Nazwisko autora musi mieć co najmniej 1 znak.")]
         [JsonPropertyName("last_name")] // Zgodne z testem JS
         public string? LastName { get; set; }
