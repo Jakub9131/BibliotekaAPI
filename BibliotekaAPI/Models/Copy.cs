1bbe4c3120a4481f842458780d3073dac7b067d8
Jakub Krzemień <jakub9141@gmail.com>
2025-12-15 09:25:38 +0100

    Update Copy.cs

--- BibliotekaAPI/Models/Copy.cs
+++ BibliotekaAPI/Models/Copy.cs
@@ -6,11 +6,9 @@ namespace BibliotekaAPI.Models
     {
         public int Id { get; set; }
 
-        // Dodatkowe opcjonalne pole, np. status egzemplarza
         public string Status { get; set; } = "Dostępny";
 
-        // Relacja N:1 (Wiele Egzemplarzy ma jedną Książkę)
-        public int BookId { get; set; } // Foreign Key
-        public Book Book { get; set; } = null!; // Obiekt nawigacyjny
+        public int BookId { get; set; } 
+        public Book Book { get; set; } = null!; 
     }
 }
