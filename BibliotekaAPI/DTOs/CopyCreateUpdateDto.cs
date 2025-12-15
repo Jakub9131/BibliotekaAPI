85b3a6c8c7d3bc7250755cce28534d019f706e83
Jakub Krzemień <jakub9141@gmail.com>
2025-12-15 09:24:02 +0100

    Update CopyCreateUpdateDto.cs

--- BibliotekaAPI/DTOs/CopyCreateUpdateDto.cs
+++ BibliotekaAPI/DTOs/CopyCreateUpdateDto.cs
@@ -7,11 +7,9 @@ namespace BibliotekaAPI.DTOs
     {
         [Required(ErrorMessage = "ID książki jest wymagane.")]
         [Range(1, int.MaxValue, ErrorMessage = "ID książki musi być dodatnie.")]
-        // KOREKTA: Zmiana na snake_case, aby dopasować do standardu testów JS
         [JsonPropertyName("book_id")]
         public int BookId { get; set; }
 
-        // Opcjonalny status
         [JsonPropertyName("status")]
         public string? Status { get; set; }
     }
