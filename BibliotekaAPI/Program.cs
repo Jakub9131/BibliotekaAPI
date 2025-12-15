0de7a38338e138037601db848a7977c6c43cd0a3
Jakub Krzemień <jakub9141@gmail.com>
2025-12-15 09:26:27 +0100

    Update Program.cs

--- BibliotekaAPI/Program.cs
+++ BibliotekaAPI/Program.cs
@@ -4,14 +4,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --------------------------------------------------------
-// JAWNA KONFIGURACJA HOSTÓW (Potwierdzona poprawnoœæ)
-// --------------------------------------------------------
-builder.WebHost.UseUrls("http://localhost:5000"); // TYLKO HTTP 5000
 
-// --------------------------------------------------------
-// KONFIGURACJA CORS (Potwierdzona poprawnoœæ)
-// --------------------------------------------------------
+builder.WebHost.UseUrls("http://localhost:5000"); 
+
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -23,29 +19,23 @@ builder.Services.AddCors(options =>
         });
 });
 
-// --------------------------------------------------------
-// KONFIGURACJA BAZY DANYCH
-// --------------------------------------------------------
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
     throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<LibraryDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-// --------------------------------------------------------
-// KONFIGURACJA US£UG
-// --------------------------------------------------------
-builder.Services.AddControllers(); // KOREKTA: Usuniêto .AddJsonOptions(...)
+
+builder.Services.AddControllers(); 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// --------------------------------------------------------
+
 var app = builder.Build();
 
-// --------------------------------------------------------
-// PIPELINE APLIKACJI
-// --------------------------------------------------------
+
 
 if (app.Environment.IsDevelopment())
 {
